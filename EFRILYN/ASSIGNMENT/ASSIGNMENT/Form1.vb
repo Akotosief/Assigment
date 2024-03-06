08140ec1c92a9933e4ee56d08be600a13c61d2a2
Efrilyn Quindoza <147592060+Akotosief@users.noreply.github.com>
2024-03-06 21:06:38 +0800

    Add files via upload

---
+++ EFRILYN/ASSIGNMENT/ASSIGNMENT/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim username As String
+    Dim password As String
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        username = Val("TextBox1.Text")
+        username = "efrilyn"
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        password = Val("TextBox2.Text")
+        password = "quindoza"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username As String = TextBox1.Text
+        Dim password As String = TextBox2.Text
+
+        If username = "efrilyn" And password = "quindoza" Then
+            MsgBox("LOGIN SUCESS")
+        ElseIf username = "" And password = "" Then
+            MsgBox("PLEASE ENTER USERNAME AND PASSWORD")
+        ElseIf username = "" Then
+            MsgBox("PLEASE ENTER USERNAME")
+        ElseIf password = "" Then
+            MsgBox("PLEASE ENTER PASSWORD")
+        Else
+            MsgBox("WRONG USERNAME AND PASSWORD")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+End Class
---
+++ EFRILYN/ASSIGNMENT/ASSIGNMENT/Form2.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form2
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        Dim NUM1, NUM2, OP As Double
+
+        NUM1 = Val(TextBox1.Text)
+        NUM2 = Val(TextBox2.Text)
+
+        If TextBox4.Text = "+" Then
+            OP = NUM1 + NUM2
+            TextBox3.Text = OP
+        ElseIf TextBox4.Text = "-" Then
+            OP = NUM1 - NUM2
+            TextBox3.Text = OP
+        ElseIf TextBox4.Text = "*" Then
+            OP = NUM1 * NUM2
+            TextBox3.Text = OP
+        ElseIf TextBox4.Text = "/" Then
+            OP = NUM1 / NUM2
+            TextBox3.Text = OP
+        Else
+            MsgBox("Enter Operator")
+        End If
+
+
+    End Sub
+End Class
